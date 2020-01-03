fad97b88199076bae7a1e41d9de55f7e343dadb5
Ben Taussig <bpt28@cornell.edu>
2020-01-03 12:31:59 -0500

    dynamic port assignment

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, url_for, Response
 import json
 from solver import solvePuzzle, solverWrapper
 from config import Config
+import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -58,4 +59,5 @@ def makeResp(body):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
